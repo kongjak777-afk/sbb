824e19f0fb5495f41cf731d19d4f69cb2c1bd3a7
kongjak777-afk <kongjak777@gmail.com>
2025-12-17 15:13:08 +0900

    모델을 사용한 데이터 웹 출력

--- src/main/java/com/mysite/sbb/question/QuestionRepository.java
+++ src/main/java/com/mysite/sbb/question/QuestionRepository.java
@@ -1,7 +1,9 @@
-package com.mysite.sbb;
+package com.mysite.sbb.question;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface QuestionRepository extends JpaRepository<Question,Integer> {
+
+
 
 }
