9479472f6e18ea2b3fe8d2fcb7cddaef9562547f
kongjak777-afk <kongjak777@gmail.com>
2025-12-29 15:25:24 +0900

    회원가입

--- src/main/java/com/mysite/sbb/SecurityConfig.java
+++ src/main/java/com/mysite/sbb/SecurityConfig.java
@@ -17,14 +17,14 @@ public class SecurityConfig {
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {           //CSRF 예외사항 등록
         http
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/**").permitAll())
+                        .requestMatchers("/**").permitAll())   // permitAll() 아무나 접근가능하게 풀어놓음
                 .csrf((csrf) -> csrf                //h2-console 예외처리
                         .ignoringRequestMatchers(("/h2-console/**")))
                 .headers((headers) -> headers
                         .addHeaderWriter(new XFrameOptionsHeaderWriter(
                                 XFrameOptionsHeaderWriter.XFrameOptionsMode.SAMEORIGIN))) ;
 
-        return http.build();
+        return http.build();   // 보안 설정을 마칠 때 사용하는 최종 빌드 메서드  http 는 아무이름이나 위에서 선언한것
     }
     // 스프링 컨테이너가 관리 하도록 빈을 올림
     @Bean
--- src/main/java/com/mysite/sbb/user/UserCreateForm.java
+++ src/main/java/com/mysite/sbb/user/UserCreateForm.java
@@ -21,7 +21,7 @@ public class UserCreateForm {
     public String password2;
 
     @NotBlank(message = "이메일은 필수 항목입니다.")
-    @Email(message = "이메일 형식이 올바르지 않습니다.")
+    @Email
     public String email;
 
 
